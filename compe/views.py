d3746ed11db92076e2262e5edf4729b65f320c21
maverick6130 <daror6130@gmail.com>
2020-12-07 23:07:47 +0530

    contests work with c++ and python

--- compe/views.py
+++ compe/views.py
@@ -1,11 +1,12 @@
 import os
-from django.db.models import indexes
 from rest_framework.decorators import api_view
 from django.http.response import JsonResponse
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_404_NOT_FOUND
+from django.core.files.storage import FileSystemStorage
 
 from .models import Contest
-from .serializers import InfoSerializer
+from .serializers import InfoSerializer, ContestSerializer
 
 # Create your views here.
 
@@ -32,15 +33,110 @@ def newcontest(request):
             contest.output.save(output.name, output)
         except:
             return JsonResponse("error", status=HTTP_400_BAD_REQUEST)
-        return JsonResponse("success", safe=False)
+        return JsonResponse("Contest created succesfully!", safe=False)
 
 @api_view(['GET'])
 def getcontest(request,id):
     if request.method=='GET':
-        print(id)
         try:
             contest = Contest.objects.get(id=id)
         except:
             return JsonResponse("error", status=HTTP_404_NOT_FOUND)
         serializer = InfoSerializer(contest)
         return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+def runcode(request,id):
+    if request.method=='POST':
+        
+        user = request.data.get('username')
+        code = request.data.get('script')
+        lang = request.data.get('language')
+        
+        try:
+            contest = Contest.objects.get(id=id)
+        except:
+            return JsonResponse("error", status=HTTP_404_NOT_FOUND)
+        
+        os.system('mkdir ./codes/temp/{}'.format(user))
+        check = True
+
+        if lang=='c_cpp':
+            os.system('touch ./codes/temp/{}/code.cpp'.format(user))
+            c = open('./codes/temp/{}/code.cpp'.format(user),'w')
+            c.write(code)
+            c.close()
+            os.system('cp {} ./codes/temp/{}/in.txt'.format(contest.input.path,user))
+            os.system('cp {} ./codes/temp/{}/out.txt'.format(contest.output.path,user))
+            if os.system('g++ -std=c++17 ./codes/temp/{}/code.cpp -o ./codes/temp/{}/exec'.format(user,user)):
+                os.system('rm -rf ./codes/temp/{}'.format(user))
+                return JsonResponse("Compilation Error", safe=False)
+            os.system('./codes/temp/{}/exec < ./codes/temp/{}/in.txt > ./codes/temp/{}/myout.txt'.format(user,user,user))
+            if os.system('cmp ./codes/temp/{}/out.txt ./codes/temp/{}/myout.txt'.format(user,user)):
+                check=False
+
+        if lang=='python':
+            os.system('touch ./codes/temp/{}/code.py'.format(user))
+            c = open('./codes/temp/{}/code.py'.format(user),'w')
+            c.write(code)
+            c.close()
+            os.system('cp {} ./codes/temp/{}/in.txt'.format(contest.input.path,user))
+            os.system('cp {} ./codes/temp/{}/out.txt'.format(contest.output.path,user))
+            if os.system('python3 ./codes/temp/{}/code.py < ./codes/temp/{}/in.txt > ./codes/temp/{}/myout.txt'.format(user,user,user)):
+                os.system('rm -rf ./codes/temp/{}'.format(user))
+                return JsonResponse("Compilation Error", safe=False)
+            if os.system('cmp ./codes/temp/{}/out.txt ./codes/temp/{}/myout.txt'.format(user,user)):
+                check=False
+        
+        os.system('rm -rf ./codes/temp/{}'.format(user))
+        if check:
+            return JsonResponse("Your Code Worked!",safe=False)
+        else:
+            return JsonResponse("Incorrect! Try again (:",safe=False)
+
+@api_view(['POST'])
+def runfile(request,id):
+    if request.method=='POST':
+        
+        user = request.data.get('username')
+        lang = request.data.get('language')
+        code = request.FILES['script']
+
+        print(lang)
+
+        try:
+            contest = Contest.objects.get(id=id)
+        except:
+            return JsonResponse("error", status=HTTP_404_NOT_FOUND)
+        
+        os.system('mkdir ./codes/temp/{}'.format(user))
+        check = True
+
+        if lang=='c_cpp':
+            fs = FileSystemStorage(location='./codes/temp/{}'.format(user))  
+            fs.save("code.cpp", code)
+            os.system('cp {} ./codes/temp/{}/in.txt'.format(contest.input.path,user))
+            os.system('cp {} ./codes/temp/{}/out.txt'.format(contest.output.path,user))
+            if os.system('g++ -std=c++17 ./codes/temp/{}/code.cpp -o ./codes/temp/{}/exec'.format(user,user)):
+                os.system('rm -rf ./codes/temp/{}'.format(user))
+                return JsonResponse("Compilation Error", safe=False)
+            os.system('./codes/temp/{}/exec < ./codes/temp/{}/in.txt > ./codes/temp/{}/myout.txt'.format(user,user,user))
+            if os.system('cmp ./codes/temp/{}/out.txt ./codes/temp/{}/myout.txt'.format(user,user)):
+                check=False
+
+        if lang=='python':
+            fs = FileSystemStorage(location='./codes/temp/{}'.format(user))  
+            fs.save("code.py", code)
+            os.system('cp {} ./codes/temp/{}/in.txt'.format(contest.input.path,user))
+            os.system('cp {} ./codes/temp/{}/out.txt'.format(contest.output.path,user))
+            if os.system('python3 ./codes/temp/{}/code.py < ./codes/temp/{}/in.txt > ./codes/temp/{}/myout.txt'.format(user,user,user)):
+                os.system('rm -rf ./codes/temp/{}'.format(user))
+                return JsonResponse("Compilation Error", safe=False)
+            if os.system('cmp ./codes/temp/{}/out.txt ./codes/temp/{}/myout.txt'.format(user,user)):
+                check=False
+        
+        os.system('rm -rf ./codes/temp/{}'.format(user))
+        if check:
+            return JsonResponse("Your Code Worked!",safe=False)
+        else:
+            return JsonResponse("Incorrect! Try again (:",safe=False)
