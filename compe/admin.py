be3dca1c1e39715bf231cdabfa30af59f10185f7
maverick6130 <daror6130@gmail.com>
2020-12-08 10:56:34 +0530

    in working condition

--- compe/admin.py
+++ compe/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Contest
+from .models import *
 
 admin.site.register(Contest)
 admin.register(Contest)
+admin.site.register(ContestUser)
+admin.register(ContestUser)
+admin.site.register(PointsTable)
+admin.register(PointsTable)
